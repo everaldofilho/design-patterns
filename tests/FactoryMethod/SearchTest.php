a455b760f098f18261990edbeaee7692e63628fd
Everaldo da Costa Filho <everaldo.webmail@gmail.com>
2019-06-08 09:20:52 -0300

    Melhorado a os testes para um melhor aproveitamento do codigo

--- tests/FactoryMethod/SearchTest.php
+++ tests/FactoryMethod/SearchTest.php
@@ -9,25 +9,43 @@ class SearchTest extends TestCase
 {
     public function testSearchForMail()
     {
-        $search = new SearchDynamic('email');
+        $search = new SearchDynamic();
+        $search->setType('email');
         $result = $search->search("email@email.com.br");
 
-        $this->assertEquals('Pequisando o email "email@email.com.br"', $result);
+        $this->assertEquals('Pesquisando pelo Email "email@email.com.br"', $result);
     }
 
     public function testSearchForName()
     {
-        $search = new SearchDynamic('name');
+        $search = new SearchDynamic();
+        $search->setType('name');
+
         $result = $search->search("Fulando de tal");
 
-        $this->assertEquals('Pesquisando pelo nome "fulano de tal"', $result);
+        $this->assertEquals('Pesquisando pelo Nome "fulano de tal"', $result);
     }
 
     public function testSearchForIp()
     {
-        $search = new SearchDynamic('ip');
+        $search = new SearchDynamic();
+        $search->setType('ip');
+
         $result = $search->search("192.168.1.1");
 
-        $this->assertEquals('Pesquisando pelo IP "fulano de tal"', $result);
+        $this->assertEquals('Pesquisando pelo IP "192.168.1.1"', $result);
+    }
+
+    public function testSearchDynamicType()
+    {
+        $search = new SearchDynamic();
+
+        $resultIp = $search->searchAll("192.168.1.1");
+        $resultEmail = $search->searchAll("email@email.com.br");
+        $resultNome = $search->searchAll("Fulando de tal");
+
+        $this->assertEquals('Pesquisando pelo IP "192.168.1.1"', $resultIp);
+        $this->assertEquals('Pesquisando pelo Email "email@email.com.br"', $resultEmail);
+        $this->assertEquals('Pesquisando pelo Nome "Fulando de tal"', $resultNome);
     }
 }
