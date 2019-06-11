1abfbbc2471ef1f3c80c0f393241611c7229af2b
Everaldo da Costa Filho <everaldo.webmail@gmail.com>
2019-06-10 21:55:40 -0300

    Added test for class responsible for data person

---
+++ tests/Adapter/PersonTest.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Tests\Adapter;
+
+use PHPUnit\Framework\TestCase;
+use App\Adapter\Person;
+
+class PersonTest extends TestCase
+{
+    public function testRegister()
+    {
+        $register = new Person;
+        $register->setName("Fulano");
+        $register->setEmail("email@email.com");
+        $register->setPhone("(11) 9 9999-999");
+        
+        $this->assertEquals("Fulano", $register->getName());
+        $this->assertEquals("email@email.com", $register->getEmail());
+        $this->assertEquals("(99) 9 9999-999", $register->getPhone());
+    }
+}
