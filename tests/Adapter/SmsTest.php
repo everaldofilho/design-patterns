ca988fe1893fef2a39d000a79e5fa831f7df32f3
Everaldo da Costa Filho <everaldo.webmail@gmail.com>
2019-06-10 21:40:34 -0300

    Added test for class responsible for sending sms

---
+++ tests/Adapter/SmsTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Adapter;
+
+use PHPUnit\Framework\TestCase;
+
+class SmsTest extends TestCase
+{
+    public function testSend()
+    {
+        $sms = new Sms;
+        $sms->setPhone("(99) 9 9999-9999");
+        $sms->setMessage("Hello Word");
+        $return = $sms->send();
+        $this->assertTrue($return);
+    }
+}
