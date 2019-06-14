36d3df1c86e9c777653e7a13e44b82f6d1509d41
Everaldo da Costa Filho <everaldo.webmail@gmail.com>
2019-06-13 21:47:30 -0300

    Added tests of the patterns Decorator

---
+++ tests/Decorator/UserTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Decorator;
+
+use PHPStan\Testing\TestCase;
+use App\Decorator\User;
+use App\Decorator\UserEncriptNormal;
+use App\Decorator\UserEncriptArgon2;
+
+class UserTest extends TestCase
+{
+    public function testCreateUserNormal()
+    {
+        $user = new User();
+        $user->setLogin("username");
+        $user->setPassword("password");
+
+        $this->assertEquals("username", $user->getLogin());
+        $this->assertEquals("password", $user->getLogin());
+    }
+
+    public function testCreateUserPasswordHash()
+    {
+        $user = new User();
+        $user->setLogin("username");
+        $user->setPassword("password");
+
+        $user = new UserEncriptNormal($user);
+
+        $this->assertTrue(password_verify('password', $user->getPassword()));
+    }
+
+    public function testCreateUserPassword()
+    {
+        $user = new User();
+        $user->setLogin("username");
+        $user->setPassword("password");
+
+        $user = new UserEncriptArgon2($user);
+
+        $this->assertTrue(password_verify('password', $user->getPassword()));
+    }
+}
